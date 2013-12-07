c6ee028b043a0dfbaaa854f1b20388e04d8f3b19
Erica Chang <echang@kixeye.com>
2013-12-07 13:47:15 -0800

    Added 2 more Parameters Env and Type. Updated Send data to reduce number of lines of code. Before I was using an if else statement now I'm using a Dictionary and getting the url via the Key which is the enviroment set when running the program.

--- Heartbeat/pulse.py
+++ Heartbeat/pulse.py
@@ -6,9 +6,14 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("messages", help="Will generate x number of messages to send though our system", type = int)
+parser.add_argument("messages", help = "Will generate x number of messages to send though our system", type = int)
+parser.add_argument("env", help = "This will set the Enviroment Endpoint. Options include 'Dev', 'Stage', 'Prod', or Test", type = str, choices = ['Dev', 'Stage', 'Prod','Test'])
+parser.add_argument("type", help = "This will set the type of heart beat to be run. Options include 'Load', 'End', 'Error'", type = str, choices = ['Load', 'End', 'Error'])
+
+
 args = parser.parse_args()
 num_messages = args.messages
+env = args.env
 
 
 def log_data(num_messages):
@@ -17,24 +22,42 @@ def log_data(num_messages):
 	for i in range(num_messages):
 		log_payload = {}
 		log_payload['count'] = i 
+		log_payload['env'] = env  
 		log_payload['game'] = "AE"
-		log_payload['env'] = "Dev" 
+		log_payload['platform'] = "ae"
+		log_payload['session_id'] = "blah" # todo : add a number generator 
 		log_payload['heart_beat_message'] = True
+		# figure out how to make a dictionary inside a dictionary for events 
+		event_data = {}
+		event_data['utc'] = "some utc numbers"
+		event_data['guid'] = 'some guid number' #another unique number generator
+		log_payload['events'] = event_data
 		log_data.append(log_payload)
 	return log_data
 
 
+
+
 def send_data(log_message):
 	log_json = json.dumps(log_message)
-	url = "https://github.com/"
+	print log_json
+	url = {}
+	url['Dev'] = 'https://www.dev.com'
+	url['Prod'] = 'https://www.prod.com'
+	url['Stage'] = 'https://www.stage.com'
+	url['Test'] = 'https://github.com/'
 	headers = {'content-Type':'application/json'}
-	r = requests.post(url, data = log_json, headers = headers)
+	r = requests.post(url[env], data = log_json, headers = headers)
+	
 
 	status = r.status_code
 	print status 
 	return status
 
+# todo figure out what a switch statement, dictionary of functions, 
+
 # Todo - Try catch exceptions - Look at how to do error exceptions 
+
 
 def errors(status):
 	# r.status_code == requests.codes.ok 
@@ -50,18 +73,9 @@ def errors(status):
 		print "Some other Error code?"
 
 
+
 log_messages = log_data(num_messages)
 
 for log_message in log_messages:
 	status= send_data(log_message)
  	errors(status);		
-
-# print r 
-# r = requests.post(url, data=json.dumps(payload), headers=headers)
-
-# if constructing the URL query string by hand you can pass it in as a dictionary with a key value pair
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# r = requests.get("https://github.com/timeline.json", stream = True)
-# r = requests.get("http://bi-logging.sjc.kixeye.com/log")
- 
-
